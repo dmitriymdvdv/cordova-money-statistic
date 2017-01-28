31d1115699458b69e2dd2145c78ad7ba4f870dfc
Dmitriy Medvedev <dmitriymdvdv@gmail.com>
2017-01-28 10:19:58 +0300

    display first bar

--- www/js/chart.js
+++ www/js/chart.js
@@ -1,33 +1,43 @@
 ;(function ($) {
-    var ctx = $("#myChart");
-    var myChart = new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 13, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            maintainAspectRatio: false
-        }
-    });
+
+    $(document).ready(function () {
+        window.MoneyStatistic = Object.assign({
+            createChart: function (config) {
+                var ctx  = config.ctx
+                   ,type = config.type
+                   ,data = config.data
+                   ,labels = config.labels;
+                var doughnutChart = new Chart(ctx, {
+                    type: type,
+                    data: {
+                        labels: labels,
+                        datasets: [{
+                            label: '# of Votes',
+                            data: data,
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderColor: [
+                                'rgba(255,99,132,1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        maintainAspectRatio: false
+                    }
+                });
+            }
+        }, window.MoneyStatistic);
+    })
 })(jQuery);
--- www/js/money.js
+++ www/js/money.js
@@ -1,3 +1,6 @@
+var smsList = [];
+var SMS = SMS || null;
+
 function onLoad() {
     if (( /(ipad|iphone|ipod|android)/i.test(navigator.userAgent) )) {
         document.addEventListener('deviceready', initApp, false);
@@ -5,8 +8,6 @@ function onLoad() {
         updateStatus('need run on mobile device for full functionalities.');
     }
 }
-
-var smsList = [];
 
 function initApp() {
     if (!SMS) {
@@ -17,79 +18,63 @@ function initApp() {
     document.addEventListener('onSMSArrive', function (e) {
         var data = e.data;
         smsList.push(data);
-
         updateStatus('SMS arrived, count: ' + smsList.length);
-
         var divdata = $('div#data');
         divdata.html(divdata.html() + JSON.stringify(data));
-
     });
-}
-
-function update(id, str) {
-    $('div#' + id).html(str);
-}
-
-function updateStatus(str) {
-    $('div#status').html(str);
-}
-
-function updateData(str) {
-    $('div#data').html(str);
 }
 
 function listSMS() {
     if (SMS) {
         SMS.listSMS({
-            'maxCount': 5,
+            'maxCount': 7,
             'address': 'MTBANK'
         }, function (data) {
-            updateStatus('sms listed as json array');
-            var html = "<table>";
             if (Array.isArray(data)) {
-                data.forEach(function (sms) {
-                    smsList.push(sms);
-                    var date    = sms.body.match(/\d\d\/\d\d\/\d\d/);
-                    var time    = sms.body.match(/\d\d:\d\d/);
-                    var money   = sms.body.match(/(\d+\.\d+)\s(\S+)/ig);
-                    var place   = sms.body.replace(/(\r\n|\n|\r)/gm," ").match(/(?:\d+\.\d+\s\S+)([^,]*)/)[1];
-                    var oplata  = money[0];
-                    var curancy = money[0].match(/(\d+\.\d+)\s(\S+)/)[2];
-                    var ostatok = money[1];
-                    html +=
-                        "<tr>" +
-                            "<td>" + sms.address + "</td>" +
-                            "<td>" + sms.body + "</td>" +
-                            "<td>" + date + "</td>" +
-                            "<td>" + time + "</td>" +
-                            "<td>" + money + "</td>" +
-                            "<td>" + place + "</td>" +
-                            "<td>" + oplata + "</td>" +
-                            "<td>" + curancy + "</td>" +
-                            "<td>" + ostatok + "</td>" +
-                        "</tr>";
+                smsList = getParseData(data);
+                var prices = smsList.map(function (item) {
+                    return parseFloat(item.oplata);
                 });
+                var places = smsList.map(function (item) {
+                    return item.place;
+                });
+                var ctx = $("#doughnutChart");
+                MoneyStatistic.createChart({
+                    ctx: ctx,
+                    type: 'doughnut',
+                    data: prices,
+                    labels: places
+                })
             }
-            html += "</table>";
-            updateData(html);
-        }, function (err) {
-            updateStatus('error list sms: ' + err);
         });
     }
 }
 
-function startWatch() {
-    if (SMS) SMS.startWatch(function () {
-        update('watching', 'watching started');
-    }, function () {
-        updateStatus('failed to start watching');
+function getParseData(data) {
+    var smsList = [];
+
+    data.forEach(function (sms) {
+        var money = getMoney(sms.body);
+        smsList.push({
+            address: sms.address,
+            body   : sms.body,
+            date   : sms.body.match(/\d\d\/\d\d\/\d\d/),
+            time   : sms.body.match(/\d\d:\d\d/),
+            money  : money,
+            place  : sms.body.replace(/(\r\n|\n|\r)/gm," ").match(/(?:\d+\.\d+\s\S+)([^,]*)/)[1],
+            oplata : parseFloat(money[0]),
+            curancy: money[0].match(/(\d+\.\d+)\s(\S+)/)[2],
+            ostatok: money[1]
+        });
     });
+
+    return smsList;
 }
 
-function stopWatch() {
-    if (SMS) SMS.stopWatch(function () {
-        update('watching', 'watching stopped');
-    }, function () {
-        updateStatus('failed to stop watching');
-    });
+function getMoney(body) {
+    return body.match(/(\d+\.\d+)\s(\S+)/ig);
 }
+
+setTimeout(function () {
+    listSMS();
+}, 2000);
